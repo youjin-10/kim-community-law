0e2ee3a559f07dd5c0d4ac622a5b5a333502f74e
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-09-03 23:24:57 +0900

    임시로 각 페이지 링크 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col gap-4">
+        <Link href="/account">Account Page</Link>
+        <Link href="/admin">Admin Page</Link>
+        <Link href="/login">Login Page</Link>
+        <Link href="/signup">Sign Up Page</Link>
+        <Link href="/reviews">Reviews Page</Link>
+        <Link href="/reviews/company/new">Write company review</Link>
+        <Link href="/reviews/interview/new">Write interview review</Link>
+      </div>
+    </main>
   );
 }
