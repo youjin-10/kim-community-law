f082c29c01d9d550da403d1ec44ed5c7be45460b
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-09-14 20:00:36 +0900

    chore: '회원가입' 텍스트 추가

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,7 @@ import SignUpForm from "./components/signup-form";
 export default function SignUpPage() {
   return (
     <div className="max-w-md mx-auto mt-10">
-      <h1 className="text-2xl font-bold mb-5">Sign Up</h1>
+      <h1 className="text-2xl font-bold mb-5">회원가입</h1>
       <SignUpForm />
     </div>
   );
