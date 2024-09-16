7c5d87750f22ac5542c532e32672b00e88c9de8a
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-09-17 00:48:24 +0900

    feat: additional columns

--- app/api/reviews/company/route.ts
+++ app/api/reviews/company/route.ts
@@ -10,29 +10,58 @@ export async function POST(request: Request) {
 
     const {
       company_name,
+      lawyer_type,
+      employment_terms,
+      good_things,
+      other_good_things,
       overall_rating,
       work_life_balance,
       salary_benefits,
       career_growth,
+      culture_fit,
       management,
       pros,
       cons,
       additional_comments,
+      overtime_frequency,
+      overtime_comments,
+      years_of_experience,
+      salary,
+      salary_type,
     } = await request.json();
+
+    // Combine good_things and other_good_things if necessary
+    const combinedGoodThings = good_things.includes("기타")
+      ? [
+          ...good_things.filter((item: string) => item !== "기타"),
+          other_good_things,
+        ]
+      : good_things;
 
     const { data, error } = await supabase
       .from("company_reviews")
       .insert({
         user_id: userId,
         company_name,
+        lawyer_type,
+        employment_terms,
+        good_things: combinedGoodThings,
         overall_rating,
         work_life_balance,
         salary_benefits,
         career_growth,
+        culture_fit,
         management,
         pros,
         cons,
         additional_comments,
+        overtime_frequency,
+        overtime_comments,
+        years_of_experience: years_of_experience
+          ? parseInt(years_of_experience)
+          : null,
+        salary: salary ? parseInt(salary) : null,
+        salary_type,
         status: "pending", // Set the initial status to 'pending'
       })
       .select()
@@ -43,7 +72,6 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: error.message }, { status: 400 });
     }
 
-    // return NextResponse.json(data);
     return NextResponse.json({
       success: true,
       redirectTo: "/reviews/submitted",
@@ -54,7 +82,7 @@ export async function POST(request: Request) {
     }
     return NextResponse.json(
       { error: "An unexpected error occurred" },
-      { status: 500 },
+      { status: 500 }
     );
   }
 }
