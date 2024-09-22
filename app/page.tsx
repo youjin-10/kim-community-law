2627376b1c1fa067b3ff2a7a6d598bf2fa66324d
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-09-22 20:13:56 +0900

    chore: 임시 텍스트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Our App</h1>
+      <h1 className="text-4xl font-bold mb-8">메인 페이지</h1>
       <p className="text-xl text-center max-w-2xl">
-        Explore company reviews and interview experiences shared by legal
-        professionals.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
+        voluptas perspiciatis repudiandae accusamus ipsam voluptate amet non
+        sapiente, modi dolore mollitia praesentium temporibus veniam tempora
+        earum commodi? Inventore, eaque laboriosam?
       </p>
     </main>
   );
