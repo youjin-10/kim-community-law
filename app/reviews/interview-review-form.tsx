8a6f554ac13d38fff07321dd2345facdc0822f02
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-09-23 15:29:18 +0900

    chore: unnecessary import

--- app/reviews/interview-review-form.tsx
+++ app/reviews/interview-review-form.tsx
@@ -17,7 +17,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { Slider } from "@/components/ui/slider";
 import {
   Select,
   SelectContent,
