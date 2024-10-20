e66cf75b99d770056014f03ffd94ea383adfd379
youjin-10 <105618795+youjin-10@users.noreply.github.com>
2024-10-20 16:31:37 +0900

    feat: styling landing page with new UI
    - styling app/page.tsx
    - remove old navbar from root layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,76 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold mb-8">메인 페이지</h1>
-      <p className="text-xl text-center max-w-2xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus
-        voluptas perspiciatis repudiandae accusamus ipsam voluptate amet non
-        sapiente, modi dolore mollitia praesentium temporibus veniam tempora
-        earum commodi? Inventore, eaque laboriosam?
-      </p>
+    <main className="flex items-center justify-center min-h-screen p-4">
+      <div className="w-full max-w-md">
+        <div className="space-y-12 md:space-y-16 lg:space-y-20 text-center p-1 md:p-3">
+          <section className="space-y-4 md:space-y-5 lg:space-y-10">
+            <div>
+              <h1 className="text-[#111] mb-2 text-sm md:text-lg">
+                변호사를 위한 모든 것, 김변호사
+              </h1>
+              <div className="flex justify-center">
+                <Image
+                  src="/images/brandmark.svg"
+                  alt="Company Logo"
+                  width={277}
+                  height={32}
+                  sizes="(min-width: 1024px) 277px, (min-width: 768px) 210px, 170px"
+                  priority
+                  className="w-[170px] md:w-[210px] lg:w-[277px] h-auto"
+                />
+              </div>
+            </div>
+            <div className="bg-[#F8F8FA] rounded-xl px-4 sm:px-9 py-2">
+              <div className="flex text-xs md:text-sm ld:text-base justify-between">
+                <div className="font-[#112D4E] font-semibold">9/00 현재</div>
+                <div>전체 회원수: 1234명</div>
+                <div>전국 변호사 가입률 60%</div>
+              </div>
+            </div>
+          </section>
+
+          <section className="space-y-4">
+            <Link href="/login">
+              <Button className="w-52 md:w-64 bg-[#112F4E]" size="lg">
+                로그인
+              </Button>
+            </Link>
+            <p className="text-xs md:text-sm text-muted-foreground">
+              아직 계정이 없으신가요?{" "}
+              <Link
+                className="font-medium hover:underline text-[#111]"
+                href="/signup"
+              >
+                회원가입
+              </Link>
+            </p>
+          </section>
+
+          <section>
+            <div className="flex justify-center items-center space-x-2 text-xs md:text-sm text-[#111]">
+              <Link className="hover:underline" href="#">
+                채용안내
+              </Link>
+              <span className="text-border">|</span>
+              <Link className="hover:underline" href="#">
+                파트너십
+              </Link>
+              <span className="text-border">|</span>
+              <Link className="hover:underline" href="#">
+                강사신청
+              </Link>
+              <span className="text-border">|</span>
+              <Link className="hover:underline" href="#">
+                문의하기
+              </Link>
+            </div>
+          </section>
+        </div>
+      </div>
     </main>
   );
 }
